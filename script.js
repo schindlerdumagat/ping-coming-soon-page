87d55c6d00a353201f0069b0e24d2ee019cf7471
schindlerwork <schindler@xtendly.com>
2025-10-21 10:06:46 +0800

    add: form handling

---
+++ script.js
@@ -0,0 +1,45 @@
+const form = document.querySelector(".form");
+const emailInput = document.querySelector(".form__email");
+const emailFeedback = document.querySelector(".form__feedback");
+const submitButton = document.querySelector(".form__submit");
+
+const validEmailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    // Extracting data from form
+    const formData = new FormData(e.target);
+    const { email } = Object.fromEntries(formData.entries());
+
+    form.classList.remove("form--error");
+    form.classList.remove("form--success");
+
+    const emailError = validateEmail(email);
+
+    if (emailError) {
+
+        // To do if email is invalid
+        form.classList.add("form--error");
+        emailFeedback.textContent = emailError;
+    } else {
+
+        // To do if email is valid
+        form.classList.add("form--success");
+        emailFeedback.textContent = "Email is submitted";
+    }
+})
+
+// Checks email input errors
+function validateEmail(email) {
+
+    let emailError = "";
+
+    if (email.length === 0) {
+        emailError = "Whoops! It looks like you forgot to add your email";
+    } else if (!validEmailRegex.test(email)) {
+        emailError = "Please provide a valid email address";
+    }
+
+    return emailError;
+}
